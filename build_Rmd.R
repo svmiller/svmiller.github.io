1ddc51e4e145c1155fae59c79d1530a340f891a7
svmiller <steven.v.miller@gmail.com>
2021-03-20 09:59:49 -0400

    for later

--- build_Rmd.R
+++ build_Rmd.R
@@ -2,3 +2,6 @@
 # https://github.com/kenkellner/blog/blob/master/build_Rmd.R
 args = commandArgs(trailingOnly = TRUE)
 knitr::knit(args[1],output=paste(args[2],sep=''))
+
+# rmarkdown::render(args[1],output_file=paste('../',args[2],sep=''))
+# ^ I still want to figure out a render option here, but I have that oddball Jekyll tag for my lead images.
